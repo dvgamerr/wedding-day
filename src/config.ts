c6e52823695886841917b65e13256e3dec230e92
Kananek Thongkam <info.dvgamer@gmail.com>
2023-01-31 20:46:38 +0700

    update meta

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'กิ่งกาญจน์ & กัณฑ์อเนก';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const widding = {
+  title: 'มาร่วมแสดงความยินดีกับเรา!',
+  time:  '4 มีนาคม พ.ศ. 2566',
+};
+export const SITE_DESCRIPTION = `${widding.title} ในวันที่ ${widding.time}`;
