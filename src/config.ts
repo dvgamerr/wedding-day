4ca13751786681af9cb663f093f373a93fbf9671
Kananek Thongkam <info.dvgamer@gmail.com>
2023-02-05 01:01:07 +0700

    update wedding time

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
-// Place any global data in this file.
+import dayjs from 'dayjs'
+import 'dayjs/locale/th'
+
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'กิ่งกาญจน์ & กัณฑ์อเนก';
-export const widding = {
+export const wedding = {
   title: 'มาร่วมแสดงความยินดีกับเรา!',
-  time:  '4 มีนาคม พ.ศ. 2566',
+  time1: dayjs('2023-03-04T00:00:00.000+0700').locale('th').add(543, 'year'),
+  time2: dayjs('2023-03-25T00:00:00.000+0700').locale('th').add(543, 'year'),
 };
-export const SITE_DESCRIPTION = `${widding.title} ในวันที่ ${widding.time}`;
+
+export const SITE_DESCRIPTION = `${wedding.title} ใน${wedding.time1.format('วันที่ DD MMMM พ.ศ. YYYY')}`;
