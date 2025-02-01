ced5ac88b59653b4032e533d1a2e484dcceca8d8
Kananek Thongkam <info.dvgamer@gmail.com>
2025-02-01 22:06:48 +0700

    fix: update site URLs and add new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wedding.touno.io',
+	site: 'https://wedding.dvgamerr.app',
 	integrations: [mdx(), sitemap()],
 });
